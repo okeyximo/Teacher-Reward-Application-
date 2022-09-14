0feedb46d1a70d6fad6e924cba4179170f62dc23
seun-codes <elizabethogundana2@gmail.com>
2022-09-14 08:33:21 +0100

    Notification service, NotificationService Implimentation

---
+++ src/main/java/com/example/rewardyourteachersq011bjavapode/repository/NotificationRepository.java
@@ -0,0 +1,2 @@
+package com.example.rewardyourteachersq011bjavapode.repository;public interface NotificationRepository {
+}
---
+++ src/main/java/com/example/rewardyourteachersq011bjavapode/serviceImpl/NotificationServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.rewardyourteachersq011bjavapode.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    private final Noti
+}
