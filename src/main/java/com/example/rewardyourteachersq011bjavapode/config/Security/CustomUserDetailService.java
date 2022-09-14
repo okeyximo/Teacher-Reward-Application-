65b980c2d00a874808d170c3bbe2fa14c091d2b6
Vinlyreeks <enwerevincent@gmail.com>
2022-09-14 03:09:28 +0100

    Code Cleanup 2

--- src/main/java/com/example/rewardyourteachersq011bjavapode/config/Security/CustomUserDetailService.java
+++ src/main/java/com/example/rewardyourteachersq011bjavapode/config/Security/CustomUserDetailService.java
@@ -16,11 +16,6 @@ public class CustomUserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-//    @Autowired
-//    public CustomUserDetailService(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findUserByEmail(username)
