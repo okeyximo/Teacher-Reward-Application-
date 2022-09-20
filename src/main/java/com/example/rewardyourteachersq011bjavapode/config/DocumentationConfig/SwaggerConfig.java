c38c9dd320ac47428272b3ba9138b75e3d954a0c
FABIAN EMMANUEL <emmanuel.fabian.dev@gmail.com>
2022-09-20 20:05:18 +0100

    fix swagger doc

--- src/main/java/com/example/rewardyourteachersq011bjavapode/config/DocumentationConfig/SwaggerConfig.java
+++ src/main/java/com/example/rewardyourteachersq011bjavapode/config/DocumentationConfig/SwaggerConfig.java
@@ -1,71 +1,55 @@
 package com.example.rewardyourteachersq011bjavapode.config.DocumentationConfig;
 
 
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Contact;
-//import io.swagger.v3.oas.models.info.Info;
-//import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.*;
+import org.springframework.util.StringUtils;
 
 @Configuration
-@EnableSwagger2
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+public class SwaggerConfig{
+    private final String moduleName;
+    private final String apiVersion;
 
-public class SwaggerConfig implements WebMvcConfigurer{
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-
-    private ApiInfo apiInfo() {
-        return new ApiInfo("REWARD YOUR TEACHER",
-                "An app that help students reward their teacher",
-                "1.0",
-                "Terms of service",
-                new Contact("test", "www.org.com", "test@emaildomain.com"),
-                "License of API",
-                "API license URL",
-                Collections.emptyList());
+    public SwaggerConfig(
+            @Value("${api.module-name}") String moduleName,
+            @Value("${api.version}") String apiVersion) {
+        this.moduleName = moduleName;
+        this.apiVersion = apiVersion;
     }
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.rewardyourteachersq011bjavapode"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String apiTitle = String.format("%s API", StringUtils.capitalize(moduleName));
+        final String apiDescription = "An app that help students reward their teacher";
+        final io.swagger.v3.oas.models.info.Contact apiContact = new Contact().name("Sq011B Pod E").url("www.org.com").email("squad11b-pod-e@gmail.com");
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(new Info()
+                        .title(apiTitle)
+                        .description(apiDescription)
+                        .termsOfService("Terms of service")
+                        .contact(apiContact)
+                        .version(apiVersion));
     }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT",AUTHORIZATION_HEADER, "header");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .build();
-    }
-
-    private List<SecurityReference> defaultAuth(){
-        AuthorizationScope authorizationScope = new AuthorizationScope("global","accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
-
-
-
-
 }
