04785c88b4850f8cfbbac895d62d6db92767747b
seun-codes <elizabethogundana2@gmail.com>
2022-09-22 17:03:53 +0100

    all issues resolved.

--- src/main/java/com/example/rewardyourteachersq011bjavapode/models/Teacher.java
+++ src/main/java/com/example/rewardyourteachersq011bjavapode/models/Teacher.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
