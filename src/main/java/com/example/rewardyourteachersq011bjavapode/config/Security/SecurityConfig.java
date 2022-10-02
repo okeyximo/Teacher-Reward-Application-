2ed4d3d3568d47e64d20d6b3526dfe3ceb446ffd
Onwuchekwa Okechukwu <xcellnximo57@gmail.com>
2022-10-02 20:18:50 +0100

    configured cross origin

--- src/main/java/com/example/rewardyourteachersq011bjavapode/config/Security/SecurityConfig.java
+++ src/main/java/com/example/rewardyourteachersq011bjavapode/config/Security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,9 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 @Configuration
 @EnableWebSecurity
